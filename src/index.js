2ab3fab9aadeb5df630f3dfc1b6624cf5abc37c2
VITALI STARASTSENKA <existing9@mail.ru>
2023-01-13 13:12:31 +0300

    feat: add index script file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function toReadable (number) {
-  
+    var ones = ['', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen'];
+    var tens = ['', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety'];
+    var hundred = ' hundred';
+    var output = '';
+    var numString = number.toString();
+
+    if(number === 0)  output = " zero";
+    else if (number < 20) {
+        output = ones[number]; 
+    }
+    else if (numString.length == 3) {
+            output = ones[parseInt(numString.charAt(0))] + hundred;
+            if(parseInt(numString.charAt(1))<2) {
+                output += ones[parseInt(numString.charAt(1)+numString.charAt(2))];
+            }
+            else {
+                output += tens[parseInt(numString.charAt(1))];
+                output += ones[parseInt(numString.charAt(2))];
+            }
+            }
+    else {
+            output += tens[parseInt(numString.charAt(0))];
+            output += ones[parseInt(numString.charAt(1))];
+        }
+return output.slice(1);
 }
+
+
